d78336124d6d2d755f1e748aa62069da2b96757e
Sidlockis <sidlockis@Sidlockiss-MacBook-Pro.local>
2026-03-19 16:03:22 -0400

    Disable caching for films page

--- app/films/page.tsx
+++ app/films/page.tsx
@@ -1,6 +1,9 @@
 import { client } from "@/lib/sanity";
 import FilmsClient from "./FilmsClient";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 const filmsQuery = `*[
   _type == "project" && "films" in tags
 ] | order(sortOrder asc, year desc){
@@ -14,7 +17,7 @@ const filmsQuery = `*[
 }`;
 
 export default async function FilmsPage() {
-  const projects = await client.fetch(filmsQuery);
+  const projects = await client.fetch(filmsQuery, {}, { cache: "no-store" });
 
   return <FilmsClient projects={projects} />;
 }
